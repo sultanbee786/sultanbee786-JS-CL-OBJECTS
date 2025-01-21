4f298a7402c9f1c965d283b5f1c1a4f7004ef43a
sultanbee786 <sulthanbeeshaik31@gmail.com>
2025-01-21 20:32:25 +0530

    Initial commit

---
+++ src/test/java/SeleniumTest.java
@@ -0,0 +1,49 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+
+import java.io.File;
+
+public class SeleniumTest {
+    private WebDriver webDriver;
+
+    @Before
+    public void setUp() {
+        // Set up ChromeDriver path
+        System.setProperty("webdriver.chrome.driver", "driver/chromedriver");//linux_64
+
+        // Get file
+        File file = new File("src/main/Objects.html");
+        String path = "file://" + file.getAbsolutePath();
+
+        // Create a new ChromeDriver instance
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("headless");
+        webDriver = new ChromeDriver(options);
+
+
+        // Open the HTML file
+        webDriver.get(path);
+    }
+
+    @After
+    public void tearDown() {
+        // Close the browser
+        webDriver.quit();
+    }
+
+    @Test
+    public void testPageText() throws InterruptedException {
+        WebElement content = webDriver.findElement(By.id("content"));
+        Assert.assertEquals("username: user1\npassword: password123", content.getText());
+    }
+
+
+}
